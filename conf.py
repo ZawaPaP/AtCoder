36b8104956dbb4d19c0ac9b2c19cbe27f403177d
ZawaPaP <cqcsolid@gmail.com>
2024-10-05 23:10:55 +0900

    test on 202041005

---
+++ conf.py
@@ -0,0 +1,51 @@
+# get input as integer
+N = int(input())
+
+# get two integers separated by space
+H, W = map(int, input().split())
+
+
+# get input as list of integers
+A = list(map(int, input().split()))
+
+# get 2次元行列
+
+N = int(input())
+matrix = []
+
+for _ in range(N):
+    row = list(map(int, input().split()))
+    matrix.append(row)
+
+# matrix には2次元行列が格納されている
+print(matrix)
+
+# alfabet
+C = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
