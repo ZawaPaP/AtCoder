e4158d425bf3e737b3222c52bd357f557125a603
Kazu <64197690+ZawaPaP@users.noreply.github.com>
2025-08-21 08:14:35 +0900

    20250821

--- Random/abc144_d.py
+++ Random/abc144_d.py
@@ -1,1 +1,14 @@
+import math
+
 a, b, x = map(int, input().split())
+
+# S >= a * a * b / 2の時、水は台形
+# そうでないとき水は三角形
+
+rad = 0
+if a * a * b / 2 >= x:
+    rad = math.atan(a * b * b / (2 * x))
+else:
+    rad = math.atan((2 * b - 2 * x / a / a) / a)
+
+print(rad / math.pi * 180)
