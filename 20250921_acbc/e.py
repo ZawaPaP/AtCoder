f412a507bfc0aa858b887c0548a024a96bced316
Kazu <64197690+ZawaPaP@users.noreply.github.com>
2025-09-22 20:21:35 +0900

    20250822

--- 20250921_acbc/e.py
+++ 20250921_acbc/e.py
@@ -4,17 +4,25 @@ T = int(input())
 ans = []
 for _ in range(T):
     N, K, X = map(int, input().split())
-    A = [-1 * a for a in list(map(float, input().split()))]
+    A = list((float(-1 * int(a)), 1) for a in input().split())
+
     heapq.heapify(A)
+
     while K > 0:
-        a = heapq.heappop(A)
-        heapq.heappush(A, a / 2)
-        heapq.heappush(A, a / 2)
-        K -= 1
+        l, c = heapq.heappop(A)
+        if c <= K:
+            K -= c
+            heapq.heappush(A, (l / 2, c * 2))
+        else:
+            heapq.heappush(A, (l / 2, K * 2))
+            heapq.heappush(A, (l, c - K))
+            K = 0
+
     while X > 0:
-        heapq.heappop(A)
-        X -= 1
-    ans.append(heapq.heappop(A))
+        l, c = heapq.heappop(A)
+        X -= c
+
+    ans.append(l * -1)
 
 for a in ans:
-    print(a * -1)
+    print(a)
